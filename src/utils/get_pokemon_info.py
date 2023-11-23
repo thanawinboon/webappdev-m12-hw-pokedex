e3941bf96cf15eba5177f7ce90602e8897259df0
thanawinboon <pokepat27@gmail.com>
2023-11-24 00:28:54 +0700

    update pokemon list

--- src/utils/get_pokemon_info.py
+++ src/utils/get_pokemon_info.py
@@ -1,10 +1,11 @@
 import requests
-import json 
+from firebase_admin import credentials, firestore, initialize_app
 # credits: https://pokeapi.co/docs/v2
 
 pkm_obj_list = []
 starting_id = 1
-ending_id = 151
+ending_id = 10
+print("Starting")
 for id in range(starting_id, ending_id + 1):
     print(f"Getting: {id}... ", end="")
     response: dict = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}").json()
@@ -26,12 +27,24 @@ for id in range(starting_id, ending_id + 1):
         "types": types,
         "evolves_from": extra_response.get("evolves_from_species")
     }
-
     pkm_obj_list.append(pkm_obj)
-    print(" DONE!")
+    print("  DONE!")
 
-pkm_list = {"pokemons": pkm_obj_list}
 
-with open("./pokedex.json", "w", encoding='utf8') as file:
-    result = json.dumps(pkm_list, indent=4, ensure_ascii=False)
-    file.write(result)
+
+# Initialize Firebase Admin SDK with your credentials file
+cred = credentials.Certificate('src/utils/firebase_credentials.json')
+initialize_app(cred)
+
+db = firestore.client()
+batch = db.batch()
+
+# gets collection called "pokemons"
+collection_ref = db.collection('pokemons')
+
+for pkm in pkm_obj_list:
+    num = str(pkm['id'])
+    id = num.zfill(3)
+    doc_ref = collection_ref.document(id)  # Manually set document ID
+    batch.set(doc_ref, pkm)
+batch.commit()
